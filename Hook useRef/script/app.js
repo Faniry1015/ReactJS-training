cd7c66d03b6110a09ddd618d715582af0a3f8db4
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-09-21 11:01:04 +0300

    useRef

--- Hook useRef/script/app.js
+++ Hook useRef/script/app.js
@@ -1,9 +1,25 @@
 function Form() {
+    const data = React.useRef(null)
+    const increment = React.useRef({count: 0})
 
+    const handleClick = function(e) {
+        e.preventDefault()
+        console.log(data.current)
+        console.log(data.current.value) 
+    }
+
+    const handleClick2 = function(e) {
+        e.preventDefault()
+        console.log(increment.current)
+        increment.current.count++
+        console.log(increment.current.count)
+    }
 
     return <form>
-        <input type="text" name="mon-text" ref={""}></input>
-        <button>Récupérer le texte saisie</button>
+        <input type="text" name="mon-text" ref={data}></input>
+        <button onClick={handleClick}>Récupérer le texte saisie</button>
+        <hr/>
+        <button onClick={handleClick2}>Incrémenter</button>
     </form>
 }
 
