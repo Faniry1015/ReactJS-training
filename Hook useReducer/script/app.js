73ab5e85e13d9ef024d657c1f16982470a355199
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-09-22 08:36:21 +0300

    useReducer

--- Hook useReducer/script/app.js
+++ Hook useReducer/script/app.js
@@ -1,12 +1,18 @@
+function init(initialValue) {
+   return {count: initialValue}
+}
+
 function reducer(state, action) {
    switch(action.type) {
       case "increment" : 
-         return state + 1
+         return {count: state.count + 1}
       case "decrement" : 
          if (state <= 0) {
-            return state = 0
+            return state
          }
-         return state - 1
+         return {count: state.count - 1}
+      case "reinit" : 
+         return init(0)
       default : 
       throw new Error("L'action" + action.type + "contient une erreur")
    } 
@@ -14,12 +20,14 @@ function reducer(state, action) {
 
 
 function Incrementer() {
-   const [count, dispatch] = React.useReducer(reducer, 0)
+   const [count, dispatch] = React.useReducer(reducer, 0, init)
 
    return <div>
-      <h1>Chiffre à modifier : {count}</h1>
+   {JSON.stringify(count)}
+      <h1>Chiffre à modifier : {count.count}</h1>
       <button onClick={() => dispatch({type: "decrement"})}>Décrementer</button>
       <button onClick={() => dispatch({type: "increment"})}>Incrémenter</button>
+      <button onClick={() => dispatch({type: "reinit"})}>Réinitialiser</button>
    </div>
 }
 
